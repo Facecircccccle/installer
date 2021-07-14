0adb9b884367dd38c4e9c8eaf73a08dd45a12f88
Vans7 <wen470366661@163.com>
2021-07-14 17:29:55 +0800

    1、VIP verify 2、odd numbers of Masters

--- pkg/setup/inputCheck.go
+++ pkg/setup/inputCheck.go
@@ -18,10 +18,10 @@ func AddRoleCheck(form *tview.Form, isHA bool) (b bool, s string) {
 	if !util.SshSuccess(ip) {
 		return false, "IP can not be reached, check ssh connect"
 	}
-	if isHA{
+	if isHA {
 		netCard := form.GetFormItemByLabel("Network Card").(*tview.InputField).GetText()
-		if !util.CheckNetCard(ip, netCard){
-			return false, "Can not find target net card named " +  "" + " on remote server, please use 'ip addr' to check."
+		if !util.CheckNetCard(ip, netCard) {
+			return false, "Can not find target net card named " + "" + " on remote server, please use 'ip addr' to check."
 		}
 	}
 	return true, ""
@@ -31,11 +31,14 @@ func InputRoleBackCheck(setup *Setup, isHA bool) (bool, string) {
 	if setup.MasterCount < 1 {
 		return false, "The cluster must have one master at least."
 	}
-	if setup.MasterCount > 1 && !isHA{
+	if setup.MasterCount > 1 && !isHA {
 		return false, "This is single master cluster part, make sure the master number is 1. If you still need more masters, choose BACK and use HA setup."
 	}
-	if setup.MasterCount == 1 && isHA{
+	if setup.MasterCount == 1 && isHA {
 		return false, "Multiple masters are typically used in the HA cluster. If you need only one master, choose BACK and use Basic setup."
+	}
+	if (setup.MasterCount%2) == 0 && isHA {
+		return false, "According to ETCD fault tolerance requirements, an odd number of master machines are required."
 	}
 	return true, ""
 }
@@ -43,7 +46,7 @@ func InputRoleBackCheck(setup *Setup, isHA bool) (bool, string) {
 func EtcdAndKubernetesVersionCheck(e *Etcds, s *Setup) (bool, string) {
 	_, etcdVersion := e.GetFormItemByLabel("Version").(*tview.DropDown).GetCurrentOption()
 
-	if util.StringIsInArray(etcdVersion, version.GetComponentVersion()[s.Kubernetes.Version].EtcdVersion){
+	if util.StringIsInArray(etcdVersion, version.GetComponentVersion()[s.Kubernetes.Version].EtcdVersion) {
 		return true, ""
 	}
 	return false, "In kubernetes " + s.Kubernetes.Version + ", etcd version should be " + strings.Join(version.GetComponentVersion()[s.Kubernetes.Version].EtcdVersion, ", ")
@@ -52,7 +55,7 @@ func EtcdAndKubernetesVersionCheck(e *Etcds, s *Setup) (bool, string) {
 func DockerAndKubernetesVersionCheck(d *Dockers, s *Setup) (bool, string) {
 	_, dockerVersion := d.GetFormItemByLabel("Version").(*tview.DropDown).GetCurrentOption()
 
-	if util.StringIsInArray(dockerVersion, version.GetComponentVersion()[s.Kubernetes.Version].DockerVersion){
+	if util.StringIsInArray(dockerVersion, version.GetComponentVersion()[s.Kubernetes.Version].DockerVersion) {
 		return true, ""
 	}
 	return false, "In kubernetes " + s.Kubernetes.Version + ", docker version should be " + strings.Join(version.GetComponentVersion()[s.Kubernetes.Version].DockerVersion, ", ")
@@ -60,10 +63,10 @@ func DockerAndKubernetesVersionCheck(d *Dockers, s *Setup) (bool, string) {
 
 func NetworkingCheck(networking *NetWorkings) (b bool, s string) {
 	var reason = ""
-	if !util.IsIpWithSubnet(networking.GetFormItemByLabel("PodSubnet").(*tview.InputField).GetText()){
+	if !util.IsIpWithSubnet(networking.GetFormItemByLabel("PodSubnet").(*tview.InputField).GetText()) {
 		return false, "Illegal Pod Subnet address"
 	}
-	if !util.IsIpWithSubnet(networking.GetFormItemByLabel("ServiceSubnet").(*tview.InputField).GetText()){
+	if !util.IsIpWithSubnet(networking.GetFormItemByLabel("ServiceSubnet").(*tview.InputField).GetText()) {
 		return false, "Illegal Service Subnet address"
 	}
 	return true, reason
@@ -71,22 +74,25 @@ func NetworkingCheck(networking *NetWorkings) (b bool, s string) {
 
 func ClusterInfoCheck(c *Clusters, setup *Setup) (b bool, s string) {
 	var reason = ""
-	if !util.IsIp(c.GetFormItemByLabel("ControllerManager_bind_address").(*tview.InputField).GetText()){
+	if !util.IsIp(c.GetFormItemByLabel("ControllerManager_bind_address").(*tview.InputField).GetText()) {
 		return false, "Illegal controller manager bind address"
 	}
-	if !util.IsIp(c.GetFormItemByLabel("Scheduler_address").(*tview.InputField).GetText()){
+	if !util.IsIp(c.GetFormItemByLabel("Scheduler_address").(*tview.InputField).GetText()) {
 		return false, "Illegal Scheduler address"
 	}
 
 	if setup.MasterCount > 1 {
-		if c.GetFormItemByLabel("Virtual_IP").(*tview.InputField).GetText() == ""{
+		if c.GetFormItemByLabel("Virtual_IP").(*tview.InputField).GetText() == "" {
 			return false, "If the number of masters is greater than 1, Virtual_IP is needed."
 		}
-		if !util.IsIp(c.GetFormItemByLabel("Virtual_IP").(*tview.InputField).GetText()){
+		if !util.IsIp(c.GetFormItemByLabel("Virtual_IP").(*tview.InputField).GetText()) {
 			return false, "Illegal Virtual IP address"
 		}
-
-		if c.GetFormItemByLabel("CertSANs").(*tview.InputField).GetText() == ""{
+		//检查vip 和master处于同一子网
+		if !util.IsVipInSameSubnet(c.GetFormItemByLabel("Virtual_IP").(*tview.InputField).GetText(), setup.Masters[0].IPAddr, util.GetNetmask(setup.Masters[0].NetCard)) {
+			return false, "Virtual IP address  needs to be in the same subnet with Maters"
+		}
+		if c.GetFormItemByLabel("CertSANs").(*tview.InputField).GetText() == "" {
 			return false, "If the number of masters is greater than 1, CertSANs (e.g. cluster.<name>.com etc..) is needed."
 		}
 	}
@@ -94,28 +100,28 @@ func ClusterInfoCheck(c *Clusters, setup *Setup) (b bool, s string) {
 }
 
 func AllocateCheck(a *Allocates) (b bool, s string) {
-	if !util.IsDigit(a.GetFormItemByLabel("KubeReservedCPU").(*tview.InputField).GetText()){
+	if !util.IsDigit(a.GetFormItemByLabel("KubeReservedCPU").(*tview.InputField).GetText()) {
 		return false, "Illegal Kube Reserved CPU"
 	}
-	if !util.IsDigit(a.GetFormItemByLabel("SysReservedCPU").(*tview.InputField).GetText()){
+	if !util.IsDigit(a.GetFormItemByLabel("SysReservedCPU").(*tview.InputField).GetText()) {
 		return false, "Illegal Sys Reserved CPU"
 	}
-	if !util.IsDigitWithStorage(a.GetFormItemByLabel("KubeMemory").(*tview.InputField).GetText()){
+	if !util.IsDigitWithStorage(a.GetFormItemByLabel("KubeMemory").(*tview.InputField).GetText()) {
 		return false, "Illegal Kube Memory"
 	}
-	if !util.IsDigitWithStorage(a.GetFormItemByLabel("SysMemory").(*tview.InputField).GetText()){
+	if !util.IsDigitWithStorage(a.GetFormItemByLabel("SysMemory").(*tview.InputField).GetText()) {
 		return false, "Illegal Sys Memory"
 	}
-	if !util.IsDigitWithStorage(a.GetFormItemByLabel("KubeStorage").(*tview.InputField).GetText()){
+	if !util.IsDigitWithStorage(a.GetFormItemByLabel("KubeStorage").(*tview.InputField).GetText()) {
 		return false, "Illegal Kube Storage"
 	}
-	if !util.IsDigitWithStorage(a.GetFormItemByLabel("SysStorage").(*tview.InputField).GetText()){
+	if !util.IsDigitWithStorage(a.GetFormItemByLabel("SysStorage").(*tview.InputField).GetText()) {
 		return false, "Illegal Sys Storage"
 	}
-	if !util.IsDigitWithStorage(a.GetFormItemByLabel("EvictionMemory").(*tview.InputField).GetText()){
+	if !util.IsDigitWithStorage(a.GetFormItemByLabel("EvictionMemory").(*tview.InputField).GetText()) {
 		return false, "Illegal Eviction Memory"
 	}
-	if !util.IsDigitWithPercent(a.GetFormItemByLabel("EvictionNodefs").(*tview.InputField).GetText()){
+	if !util.IsDigitWithPercent(a.GetFormItemByLabel("EvictionNodefs").(*tview.InputField).GetText()) {
 		return false, "Illegal Eviction Node"
 	}
 
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -8,12 +8,13 @@ import (
 	"os/exec"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"unicode"
 )
 
 func IsIp(ip string) (b bool) {
-	if ip == ""{
+	if ip == "" {
 		return true
 	}
 	if m, _ := regexp.MatchString("^(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)$", ip); !m {
@@ -23,7 +24,7 @@ func IsIp(ip string) (b bool) {
 }
 
 func IsIpWithSubnet(ip string) (b bool) {
-	if ip == ""{
+	if ip == "" {
 		return true
 	}
 	if m, _ := regexp.MatchString("^(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)/(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)$", ip); !m {
@@ -32,6 +33,26 @@ func IsIpWithSubnet(ip string) (b bool) {
 	return true
 }
 
+//判断VIP是否和master处于同一子网
+func IsVipInSameSubnet(vip, target string, mask string) (b bool) {
+	vipnet := strings.Split(vip, ".")
+	targetnet := strings.Split(target, ".")
+	masknet := strings.Split(mask, ".")
+	//每段与掩码进行与运算
+	for i := 0; i < len(masknet); i++ {
+		m, _ := strconv.Atoi(masknet[i])
+		t, _ := strconv.Atoi(targetnet[i])
+		v, _ := strconv.Atoi(vipnet[i])
+		if (t & m) != (v & m) {
+			return false
+		}
+	}
+	return true
+}
+func GetNetmask(netcard string) string {
+	mask := ExecShell("ifconfig " + netcard + " |sed -n 2p |awk -F ' ' '{print$4}'")
+	return mask
+}
 func IsDigit(str string) bool {
 	for _, x := range []rune(str) {
 		if !unicode.IsDigit(x) {
@@ -42,20 +63,20 @@ func IsDigit(str string) bool {
 }
 
 func IsDigitWithStorage(str string) bool {
-	if(IsDigit(str[0:len(str)-2])) == false{
+	if (IsDigit(str[0 : len(str)-2])) == false {
 		return false
 	}
-	if(str[len(str)-2:]) == "Gi" || (str[len(str)-2:]) == "Mi"{
+	if (str[len(str)-2:]) == "Gi" || (str[len(str)-2:]) == "Mi" {
 		return true
 	}
 	return false
 }
 
 func IsDigitWithPercent(str string) bool {
-	if(IsDigit(str[0:len(str)-1])) == false{
+	if (IsDigit(str[0 : len(str)-1])) == false {
 		return false
 	}
-	if(str[len(str)-1:]) == "%"{
+	if (str[len(str)-1:]) == "%" {
 		return true
 	}
 	return false
@@ -117,9 +138,9 @@ func CheckNetCard(ip string, card string) bool {
 	ExecShell("sh localScript/add_ansible_host.sh " + "netCardCheck " + ip)
 	ExecShell("ansible-playbook localScript/netCardCheck.yaml")
 
-	return strings.Contains(ExecShell("cat /home/" + ip + "/home/networkCard"), card)
+	return strings.Contains(ExecShell("cat /home/"+ip+"/home/networkCard"), card)
 }
 
 func SshSuccess(ip string) bool {
-	return strings.Contains(ExecShell("ansible " + ip + " -m ping"), "SUCCESS")
+	return strings.Contains(ExecShell("ansible "+ip+" -m ping"), "SUCCESS")
 }
