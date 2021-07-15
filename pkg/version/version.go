0ee3c9197b29b4c578e8e1b371c0b09a4bdbcc7a
Vans7 <wen470366661@163.com>
2021-07-15 16:30:13 +0800

    test

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -8,37 +8,37 @@ type ComponentVersion struct {
 }
 
 var version = map[string]ComponentVersion{
-	"1.20.1": {
+	"v1.20.1": {
 		DockerVersion:  []string{"docker-ce-19.03.15"},
 		EtcdVersion:    []string{"3.4.13-0"},
 		CoreDNSVersion: []string{"1.7.0"},
 		PauseVersion:   []string{"3.2"},
 	},
-	"1.20.2": {
+	"v1.20.2": {
 		DockerVersion:  []string{"docker-ce-19.03.15"},
 		EtcdVersion:    []string{"3.4.13-0"},
 		CoreDNSVersion: []string{"1.7.0"},
 		PauseVersion:   []string{"3.2"},
 	},
-	"1.20.3": {
+	"v1.20.3": {
 		DockerVersion:  []string{"docker-ce-19.03.15"},
 		EtcdVersion:    []string{"3.4.13-0"},
 		CoreDNSVersion: []string{"1.7.0"},
 		PauseVersion:   []string{"3.2"},
 	},
-	"1.20.4": {
+	"v1.20.4": {
 		DockerVersion:  []string{"docker-ce-19.03.15"},
 		EtcdVersion:    []string{"3.4.13-0"},
 		CoreDNSVersion: []string{"1.7.0"},
 		PauseVersion:   []string{"3.2"},
 	},
-	"1.20.5": {
+	"v1.20.5": {
 		DockerVersion:  []string{"docker-ce-19.03.15"},
 		EtcdVersion:    []string{"3.4.13-0"},
 		CoreDNSVersion: []string{"1.7.0"},
 		PauseVersion:   []string{"3.2"},
 	},
-	"1.20.6": {
+	"v1.20.6": {
 		DockerVersion:  []string{"docker-ce-19.03.15"},
 		EtcdVersion:    []string{"3.4.13-0"},
 		CoreDNSVersion: []string{"1.7.0"},
